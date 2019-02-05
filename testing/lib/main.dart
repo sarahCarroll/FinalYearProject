31c78a6fea7f2ca99301e7cdd5bec6aa2901b9a7
sarahc123 <g00330821@gmit.ie>
2019-02-05 16:56:25 +0000

    dissertation

--- testing/lib/main.dart
+++ testing/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 import 'package:testing/widget.dart';
-import 'package:map_view/figure_joint_type.dart';
-import 'package:map_view/polygon.dart';
 
 var myKey = "AIzaSyDzLAc2Uos2XNBT26mMJRqQ33aHoSnF0oM";
 
@@ -16,18 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Medieval Walking Tour',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -44,10 +33,107 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // MapView mapView = new MapView();
+
+  // List<Marker> markers = <Marker>[
+  //   new Marker(
+  //     "Hall Of The Red Earl",
+  //     "Hall of the Red Earl",
+  //     53.2710,
+  //     -9.0537,
+  //   ),
+  //   new Marker("Lynch's Castle", "Lynch's Castle", 53.2722, -9.0533),
+  //   new Marker("st. nicholas' collegiate church",
+  //       "st. nicholas' collegiate church", 53.2727, -9.0539),
+  //   new Marker("Kings Head", "Kings Head", 53.2722, -9.0533),
+  //   new Marker("Eyre Square", "Eyre Square", 53.2747, -9.0500),
+  // ];
+
+  // displayMap() {
+  //   mapView.show(new MapOptions(
+  //     mapViewType: MapViewType.normal,
+  //     showUserLocation: true,
+  //     initialCameraPosition:
+  //         new CameraPosition(new Location(53.2707, -9.0568), 15.0),
+  //     title: "google map",
+  //   ));
+
+  //   mapView.onMapTapped.listen((tapped) {
+  //     mapView.setMarkers(markers);
+  //     mapView.zoomToFit(padding: 100);
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text(widget.title),
+          centerTitle: true,
+        ),
+        body: new ListView(children: <Widget>[
+          new MyImageWidget(),
+          new RaisedButton(
+              child: Text('Hall Of The Red Earl'),
+              onPressed: () {
+                //Navigator.pushNamed(context, MyHallPage.routeName);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => _MyHallPageState()),
+                );
+                //MaterialPageRoute(builder: (context) => ));
+              }),
+          new MyKingsWidget(),
+          new Container(
+            child: RaisedButton(
+                child: Text('Kings Head'),
+                elevation: 7.8,
+                onPressed: () {
+                  //Navigator.pushNamed(context, MyHallPage.routeName);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Map()),
+                  );
+                }),
+          ),
+        ]));
+  }
+}
+
+class _MyHallPageState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Hall Of the Red Earl"),
+        ),
+        body: new ListView(children: <Widget>[
+          new MyImageWidget(),
+          new Text("Hall Of The Red Earl",
+              style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15.0)),
+          new Text("This is all the information about the hall of the red earl",
+              style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0)),
+        ]));
+  }
+}
+
+class Map extends StatelessWidget {
   MapView mapView = new MapView();
 
   List<Marker> markers = <Marker>[
-    new Marker("Hall Of The Red Earm", " ", 53.2710, -9.0537)
+    new Marker(
+      "Hall Of The Red Earl",
+      "Hall of the Red Earl",
+      53.2710,
+      -9.0537,
+    ),
+    new Marker("Lynch's Castle", "Lynch's Castle", 53.2722, -9.0533),
+    new Marker("st. nicholas' collegiate church",
+        "st. nicholas' collegiate church", 53.2727, -9.0539),
+    new Marker("Kings Head", "Kings Head", 53.2722, -9.0533),
+    new Marker("Eyre Square", "Eyre Square", 53.2747, -9.0500),
   ];
 
   displayMap() {
@@ -67,78 +153,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
         appBar: new AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: new Text(widget.title),
-          centerTitle: true,
+          title: new Text("Google Maps"),
         ),
-        body: new ListView(children: <Widget>[
-          new MyImageWidget(),
-          new Container(
-            child: RaisedButton(
-              child: Text('Hall Of The Red Earl Location'),
-              color: Colors.purple,
-              elevation: 7.8,
-              onPressed: displayMap,
-            ),
-          ),
-          new MyKingsWidget(),
-          new Text("Kings Head",
-              style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0)),
-        ]));
+        body: displayMap());
   }
 }
-
-// class MyHallPage extends StatefulWidget {
-//   MyHallPage({Key key, this.title}) : super(key: key);
-
-//   static const String routeName = "/MyHallPage";
-
-//   final String title;
-
-//   @override
-//   _MyHallPageState createState() => new _MyHallPageState();
-// }
-
-// class _MyHallPageState extends State<MyHallPage> {
-//   MapView mapView = new MapView();
-
-//   displayMap() {
-//     mapView.show(new MapOptions(
-//       mapViewType: MapViewType.normal,
-//       showUserLocation: true,
-//       initialCameraPosition:
-//           new CameraPosition(new Location(53.2707, -9.0568), 15.0),
-//       title: "google map",
-//     ));
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return new Scaffold(
-//         appBar: new AppBar(
-//           // Here we take the value from the MyHomePage object that was created by
-//           // the App.build method, and use it to set our appbar title.
-//           title: new Text(widget.title),
-//         ),
-//         body: new ListView(children: <Widget>[
-//           new MyImageWidget(),
-//           new Text("Hall Of The Red Earl",
-//               style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15.0)),
-//           new Text("This is all the information about the hall of the red earl",
-//               style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0)),
-//           new RaisedButton(
-//             child: Text('Location'),
-//             onPressed: displayMap(),
-//           )
-//         ]));
-//   }
-// }
