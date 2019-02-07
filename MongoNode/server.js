7cce07a1402679ba2edebba146481550aef185b2
Abigail Cul <g00334291@gmit.ie>
2019-02-07 13:08:15 +0000

    Mongo and images

--- MongoNode/server.js
+++ MongoNode/server.js
@@ -8,7 +8,8 @@ app.listen(3000, function() {
   console.log('listening on 3000/')
 })
 
-
+// Database Name
+const dbName = 'myproject';
 
 /*app.get('/', (req, res) => {
   res.send('Hello World')
@@ -19,9 +20,10 @@ app.get('/', (req, res) => {
   // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
+//34.242.41.209/3000 ip lookup
 
 app.get("/users", function() {
-  MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+  MongoClient.connect("mongodb://localhost:27017/myproject", function(err, db) {
     if (err) next
     db
       .collection("users")
@@ -32,4 +34,24 @@ app.get("/users", function() {
         res.json(result)
       });
   });
+  
+   if (err) {
+        console.log('Sorry unable to connect to MongoDB Error:', err);
+    } else {
+ 
+        var bucket = new mongodb.GridFSBucket(db, {
+            chunkSizeBytes: 1024,
+            bucketName: 'images'
+        });
+ 
+        fs.createReadStream('C:\Users\Administrator\Desktop\explore.jpg').pipe(
+            bucket.openUploadStream('explore.jpg')).on('error', function(error) {
+            console.log('Error:-', error);
+        }).on('finish', function() {
+            console.log('File Inserted!!');
+            process.exit(0);
+        });
+    }
 });
+
+   
