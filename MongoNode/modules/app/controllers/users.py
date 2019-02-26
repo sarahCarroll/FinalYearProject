95017e8feeac63692246091201804143fa034ead
Abigail Cul <g00334291@gmit.ie>
2019-02-26 14:38:14 +0000

    modules folder

--- MongoNode/modules/app/controllers/users.py
+++ MongoNode/modules/app/controllers/users.py
@@ -3,6 +3,7 @@ import os
 from flask import request, jsonify
 from app import app, mongo
 import logger
+from flask_pymongo import PyMongo
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
@@ -17,3 +18,8 @@ def user():
         return jsonify(data), 200
 
     data = request.get_json()
+
+@app.route('/', methods=['Patch')
+def index():
+     db.info.update({'_id' : ObjectId("5c62f1b168b454424c0d8e15")}, {"body": "Hall.txt", "title": "Hall of the Red Earl", "description": "Info about hall", "filename":"explore.jpg", "size 25"})
+    return jsonify(data), 200
