16a340d2dd6285b7b24a08ddc54b35e7b3a2066d
Abigail Cul <g00334291@gmit.ie>
2019-03-13 19:16:42 +0000

    new database and dart file changes to print db

---
+++ testing/lib/info_model.dart
@@ -0,0 +1,12 @@
+class Multi_Info{
+  final List<Info> list;
+
+  Multi_Info({this.list});
+}
+
+class Info{
+  final String id;
+  final String description;
+
+  Info({this.id, this.description});
+}
