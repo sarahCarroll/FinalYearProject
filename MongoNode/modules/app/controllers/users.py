576f54163f84846f1576fdb9e78ad49c969930a8
Abigail Cul <g00334291@gmit.ie>
2019-03-22 11:44:42 +0000

    adding crud to python

--- MongoNode/modules/app/controllers/users.py
+++ MongoNode/modules/app/controllers/users.py
@@ -1,8 +1,9 @@
 ''' controller and routes for users '''
 import os
-from flask import request, jsonify
+from flask import request, jsonify, render_template,request,redirect,url_for # For flask implementation  
 from app import app, mongo
 import logger
+
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
@@ -14,11 +15,7 @@ def user():
         query = request.args
         output = []
         for a in mongo.db.info.find(query):
-<<<<<<< HEAD
             output.append({'_id': a['_id'], 'description' : a['description']})
-=======
-            output.append({'_id' : a['_id'], 'description' : a['description']})
->>>>>>> 89381cec8fc14189bddbc4d4146542ca3d3c6f5b
         
         return jsonify({'multi' : output}), 200
 
@@ -41,4 +38,42 @@ def user():
             return jsonify(response), 200
         else:
             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+ 
+# @app.route("/list")  
+# def lists ():  
+#     #Display the all Tasks  
+#     mongo.db.info.find()  
+#     a1="active"  
+#     return render_template('index.html',info=info,t=title,h=heading)  
+ 
+# @app.route("/action", methods=['POST'])  
+# def action ():  
+#     #Adding a Task  
+#     description=request.values.get("description")  
+#     mongo.db.info.insert({ "description":description,"done":"no"})  
+#     return redirect("/list")  
+ 
+# @app.route("/remove")  
+# def remove ():  
+#     #Deleting a Task with various references  
+#     key=request.values.get("_id")  
+#     mongo.db.info.remove({"_id":ObjectId(key)})  
+#     return redirect("/")  
+ 
+# @app.route("/update")  
+# def update ():  
+#     id=request.values.get("_id")  
+#     mongo.db.info.find({"_id":ObjectId(id)})  
+#     return render_template('update.html',tasks=task,h=heading,t=title)  
+ 
+# @app.route("/action3", methods=['POST'])  
+# def action3 ():  
+#     #Updating a Task with various references  
+#     description=request.values.get("description")  
+#     id=request.values.get("_id")  
+#     info.update({"_id":ObjectId(id)}, {'$set':{ "description":description }})  
+#     return redirect("/") 
 
+#     if __name__ == "__main__":  
+  
+#       app.run()
