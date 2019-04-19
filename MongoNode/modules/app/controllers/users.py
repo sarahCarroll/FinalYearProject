5b7031eed15580c49af159a009621e158366e737
abigailCul <g00334291@gmit.ie>
2019-04-19 21:55:16 +0000

    delete working

--- MongoNode/modules/app/controllers/users.py
+++ MongoNode/modules/app/controllers/users.py
@@ -7,7 +7,7 @@ from bson.objectid import ObjectId
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
-    __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
+    __name__,filename=os.path.join(ROOT_PATH, 'output.log'))
 
 @app.route('/data', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 def user():
@@ -23,14 +23,14 @@ def user():
 
     if request.method == 'POST':
         if data.get('_id', None) is not None and data.get('description', None) is not None:
-            mongo.db.users.insert_one(data)
+            mongo.db.info.insert_one(data)
             return jsonify({'ok': True, 'message': 'created successfully!'}), 200
         else:
             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
     if request.method == 'DELETE':
         if data.get('description', None) is not None:
-            db_response = mongo.db.users.delete_one({'description': data['description']})
+            db_response = mongo.db.info.delete_one({'description': data['description']})
             if db_response.deleted_count == 1:
                 response = {'ok': True, 'message': 'record deleted'}
             else:
@@ -41,7 +41,7 @@ def user():
 
     if request.method == 'PATCH':
         if data.get('query', {}) != {}:
-            mongo.db.users.update_one(
+            mongo.db.info.update_one(
                 data['query'], {'$set': data.get('multi', {})})
             return jsonify({'ok': True, 'message': 'record updated'}), 200
         else:
@@ -50,46 +50,60 @@ def user():
 
 @app.route('/new', methods=['GET'])
 def new():
-    return render_template('add.html')
+    return render_template("add.html")
 
 @app.route('/view', methods=["GET"])
-def viewing(id):
-    data = info.find_one({
-        '_id': ObjectId(id)
+def viewing():
+    data = mongo.db.info.find_one({
+    '_id': ObjectId()
+     #   '_id': data['_id']
 
         })
-    return render_template('list.html', info=data)
+    return render_template("list.html", _id=data )
 
 @app.route('/update', methods=['POST'])
-def update(id):
-    info.update_one({
-        '_id': ObjectId(id)
+def update():
+    data = mongo.db.info.update_one({
+        '_id': ObjectId()
         }, {
             '$set': {
                 'description': request.form['description'],
                 }
             })
-    return render_template('update.html', multi=data)
+    return render_template("update.html", _id=data)
 
 @app.route('/delete', methods=["POST"])
-def remove(id):
-    data = delete_many({
-        '_id': ObjectId(id)
+def remove():  
+    q = request.args
+    out =[]
+    for a in mongo.db.info.delete_one(q):
+        out({_id: a['5c62f1b168b454424c0d8e15']})
+    #dbR=  mongo.db.info.delete_one({
+     #   '_id': request.form['_id']
+      #  })
+    if dbR.deleted_count == 1:
+        response = {'ok': True, 'message': 'record deleted'}
+    else:
+        response = {'ok': True, 'message': 'no record found'}
+    return jsonify(response), 200
+        #'_id': ObjectId()
+        #})
+    #return redirect(url_for('list'))
 
-        })
-    return redirect(url_for('list'))
+
 
 
 @app.route('/add', methods=["POST"])
 def adding():
-    info.insert_one({
+    mongo.db.info.insert_one({
         '_id': request.form['_id'],
         'description': request.form['description'],         
      
         })
-    return redirect(url_for('list'))
+    return redirect(url_for("list"))
 
 
-
+if __name__ == '__main__':
+    LOG.run(debug=True)
                 
 
