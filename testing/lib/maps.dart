160ab985fdabf0342d4cd9df51751e27155198ca
sarahc123 <g00330821@gmit.ie>
2019-04-23 13:05:32 +0100

    added route to google maps

--- testing/lib/maps.dart
+++ testing/lib/maps.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:map_view/map_view.dart';
+import 'package:map_view/polygon.dart';
+import 'package:map_view/polyline.dart';
 
 class Map1 extends StatelessWidget {
   MapView mapView = new MapView();
@@ -21,33 +23,41 @@ class Map1 extends StatelessWidget {
   ];
 
   displayMap() {
-    mapView.show(new MapOptions(
-      mapViewType: MapViewType.normal,
-      showUserLocation: true,
-      initialCameraPosition:
-          new CameraPosition(new Location(53.2707, -9.0568), 15.0),
-      title: "google map",
-    ));
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            initialCameraPosition:
+                new CameraPosition(new Location(53.2707, -9.0568), 15.0),
+            title: "Recently Visited"),
+        toolbarActions: [new ToolbarAction("Close", 0)]);
+
+    List<Polygon> polygons = <Polygon>[
+      new Polygon(
+          "Nice one",
+          <Location>[
+            new Location(53.2710, -9.0537),
+            new Location(53.2722, -9.0533),
+            new Location(53.2727, -9.0539),
+            new Location(53.2732, -9.0523),
+            new Location(53.2747, -9.0500),
+            new Location(53.2738, -9.0507),
+          ],
+          //jointType: FigureJointType.round,
+          strokeColor: Colors.blue,
+          strokeWidth: 10.0,
+          fillColor: Colors.blue.withOpacity(0.1))
+    ];
 
     mapView.onMapTapped.listen((tapped) {
       mapView.setMarkers(markers);
       mapView.zoomToFit(padding: 100);
+      mapView.setPolygons(polygons);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    mapView.show(new MapOptions(
-      mapViewType: MapViewType.normal,
-      showUserLocation: true,
-      initialCameraPosition:
-          new CameraPosition(new Location(53.2707, -9.0568), 15.0),
-      title: "google map",
-    ));
-    mapView.onMapTapped.listen((tapped) {
-      mapView.setMarkers(markers);
-      mapView.zoomToFit(padding: 100);
-    });
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("press back"),
